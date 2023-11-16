3c1e651b568ac7631fe605ee6b284a2ce2aa17c6
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-11-16 18:33:17 -0300

    Condições em JavaScript

---
+++ exercicios/aula11/ex008/index.js
@@ -0,0 +1,8 @@
+var vel = 60;
+
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+if(vel > 60){ //Condição Simples (Não tem else)
+    console.log('Você ultrapassou a velocidade permitida, MULTADO');
+}
+
+console.log('Dirija sempre utilizando cinto de segurança');
