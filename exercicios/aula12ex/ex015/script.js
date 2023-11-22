6e29202944433803c02f79c4af366588a3eaad90
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-11-22 19:18:23 -0300

    Imagens do exercício 15 adicionadas

---
+++ exercicios/aula12ex/ex015/script.js
@@ -0,0 +1,20 @@
+
+/*
+Infancia
+Primeira infância: do nascimento aos 3 anos de idade;
+Segunda infância: dos 3 aos 6 anos;
+Terceira infância: dos 6 aos 11 anos.
+
+Adolescência
+Pré-adolescência ou adolescência menor: dos 11 aos 14 anos de idade;
+Adolescência média: dos 14 aos 17 anos;
+Adolescência maior ou juventude: dos 17 aos 20 anos.
+
+Adulto
+Adulto jovem: dos 21 aos 40 anos de idade;
+Meia idade: dos 40 aos 65 anos.
+
+Velhice
+Idoso jovem: dos 65 aos 75 anos de idade;
+Idoso mais velho: acima dos 75 anos.
+*/
