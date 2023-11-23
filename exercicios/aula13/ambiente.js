2c45d13faa23de7b4fa00e682879da723e3c32eb
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-11-23 17:48:41 -0300

    Exercício de repetições

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,29 @@
+//Estrutura de repetição com teste lógico no final
+var c = 1;
+do{
+    console.log(`Passo ${c}`);
+    c++     //c = c + 1;
+}while (c <= 3);
+
+
+
+/*
+//Estrutura de repetição com teste lógico no início
+
+var c = 1;
+while (c <= 3) {
+    console.log(`Passo ${c}`);
+    c++     //c = c + 1;
+}
+*/
+
+
+
+/*
+//Maneira menos adequada, mas que é possível fazer repetição. 
+
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
+
