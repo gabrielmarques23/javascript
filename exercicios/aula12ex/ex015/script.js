3d3217ccae5e4140c37abfc7f94052819b562001
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-11-23 03:28:06 -0300

    Finalizado o exercício 15

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -6,60 +6,94 @@ function verificar() {
 
     if (fAno.value.length == 0 || fAno.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente');
-    } else {
+    } else if (fAno.value <= 1899) {
+        window.alert('[ERRO] O ano de nascimento tem que ser acima de 1899!')
+    }
+    else {
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(fAno.value);
         var gênero = '';
-        var img = document.createElement('img');
-        img.setAttribute('id', 'foto');
-        if (fsex[0].checked) {
+        var img = window.document.getElementById('foto');
+
+        if (fsex[0].checked) { //importante
             gênero = 'Homem';
-            if (idade >= 0 && idade < 10) {
-                //criança
-            } else if (idade >= 10 && idade < 21) {
-                //jovem
-            } else if (idade < 50) {
-                //adulto
+            if (idade >= 0 && idade <= 3) {
+                img.src = 'imagens/bebe-m.png';
+                gênero = 'Bebê';
+
+            } else if (idade >= 4 && idade <= 8) {
+                img.src = 'imagens/crianca-m.png';
+                gênero = 'Criança';
+
+            } else if (idade >= 9 && idade <= 12) {
+                img.src = 'imagens/menino.png';
+                gênero = 'Menino';
+
+            } else if (idade >= 13 && idade <= 17) {
+                img.src = 'imagens/adolescente-m.png';
+                gênero = 'Menino';
+
+            } else if (idade >= 18 && idade <= 21) {
+                img.src = 'imagens/adolescente-20-m.png';
+                gênero = 'Homem';
+
+            } else if (idade >= 22 && idade <= 30) {
+                img.src = 'imagens/homem.png';
+                gênero = 'Homem';
+
+            } else if (idade >= 31 && idade < 50) {
+                img.src = 'imagens/homem-40.png';
+
+            } else if (idade >= 51 && idade <= 65) {
+                img.src = 'imagens/idoso-j-m.png';
+
             } else {
-                //idoso
+                img.src = 'imagens/idoso-80.png';
+
             }
+
         } else if (fsex[1].checked) {
             gênero = 'Mulher';
-            if (idade >= 0 && idade < 10) {
-                //criança
-            } else if (idade >= 10 && idade < 21) {
-                //jovem
-            } else if (idade < 50) {
-                //adulto
+            if (idade >= 0 && idade <= 3) {
+                img.src = 'imagens/bebe-f.png';
+                gênero = 'Bebê';
+
+            } else if (idade >= 4 && idade <= 8) {
+                img.src = 'imagens/crianca-f.png';
+                gênero = 'Criança';
+
+            } else if (idade >= 9 && idade <= 12) {
+                img.src = 'imagens/menina.png';
+                gênero = 'Menina';
+
+            } else if (idade >= 13 && idade <= 17) {
+                img.src = 'imagens/adolescente-f.png';
+                gênero = 'Menina';
+
+            } else if (idade >= 18 && idade <= 21) {
+                img.src = 'imagens/adolescente-20-f.png';
+                gênero = 'Mulher';
+            } else if (idade >= 22 && idade <= 30) {
+                img.src = 'imagens/mulher.png';
+                gênero = 'Mulher';
+
+            } else if (idade >= 31 && idade < 50) {
+                img.src = 'imagens/mulher-40.png';
+                gênero = 'Mulher'
+
+            } else if (idade >= 51 && idade <= 65) {
+                img.src = 'imagens/idosa-j-f.png';
+                gênero = 'Mulher'
+
             } else {
-                //idoso
+                img.src = 'imagens/idosa-80.png';
+                gênero = 'Mulher'
+
             }
         }
 
-        res.style.textAlign = 'center';
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`;
+
+        res.innerHTML = `Detectamos ${gênero} com ${idade} ano/s`;
     }
 }
 
-
-
-
-/*
-Infancia
-Primeira infância: do nascimento aos 3 anos de idade;
-Segunda infância: dos 3 aos 6 anos;
-Terceira infância: dos 6 aos 11 anos.
-
-Adolescência
-Pré-adolescência ou adolescência menor: dos 11 aos 14 anos de idade;
-Adolescência média: dos 14 aos 17 anos;
-Adolescência maior ou juventude: dos 17 aos 20 anos.
-
-Adulto
-Adulto jovem: dos 21 aos 40 anos de idade;
-Meia idade: dos 40 aos 65 anos.
-
-Velhice
-Idoso jovem: dos 65 aos 75 anos de idade;
-Idoso mais velho: acima dos 75 anos.
-*/
