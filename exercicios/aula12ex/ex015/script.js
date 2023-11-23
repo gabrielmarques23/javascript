7db303ea6cffc52893e585cf1f5304c75f5f04ad
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-11-22 21:03:51 -0300

    ex15 iniciado

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -1,3 +1,48 @@
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fAno = window.document.getElementById('txtano');
+    var res = window.document.getElementById('res');
+
+    if (fAno.value.length == 0 || fAno.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente');
+    } else {
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fAno.value);
+        var gênero = '';
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto');
+        if (fsex[0].checked) {
+            gênero = 'Homem';
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade >= 10 && idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher';
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade >= 10 && idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
+        }
+
+        res.style.textAlign = 'center';
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`;
+    }
+}
+
+
+
 
 /*
 Infancia
