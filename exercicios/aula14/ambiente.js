1430165cb42618d71ca3ec3d16d0ad7779431aca
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-11-24 18:53:55 -0300

    Exercício sobre Estrutura de repetições

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,27 @@
+console.log('Vai começar')
+for (var c = 1; c <= 4; c++) {
+    console.log(`número ${c}`);
+}
+console.log('Fim');
+
+
+
+
+
+
+
+
+/*var c = 1;
+
+while (c <= 5) {
+    console.log(c)
+    c++
+
+}*/
+//////////////////////////////////////
+/* 
+for (var c = 1; c <= 5; c++) {
+    console.log(`número ${c}`);
+}
+*/
+
