32530b32f6c2133886982b642b2b65ec2f96a134
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-04 15:54:31 -0300

    ambiente.js da aula 15 feito

--- desafios/des004/script.js
+++ desafios/des004/script.js
@@ -2,14 +2,14 @@ function comprar() {
 
 
     let pr1 = document.getElementById('prod1');
-    
+
     let pr2 = document.getElementById('prod2');
     let pr3 = document.getElementById('prod3');
     let pr4 = document.getElementById('prod4');
 
-    let vl1 = pr1 = 100;
-    let vl2 = pr2 = 200;
-    let vl3 = pr3 = 300;
-    let vl4 = pr4 = 400;
+    pr1 = 100;
+    pr2 = 200;
+    pr3 = 300;
+    pr4 = 400;
 
 }
---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 2, 7, 8, 4, 9]
+
+
+console.log(`O vetor tem os seguintes números: ${num}.`);
+console.log(`Em ordem crescente, ficam assim: ${num.sort()}.`);
+console.log(`O vetor tem ${num.length} posições`);
+console.log(`${num[0]} É o primeiro número, e o ${num[5]} é o último!`); 
