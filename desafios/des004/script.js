5759636dc9a35b601d0a1925368e16dabc64c93e
Gabriel Marques <gabrielmarques.if@gmail.com>
2023-12-05 01:21:02 -0300

    Desafio 4 finalizado
    Desafio que usei uma iterface UI mais 'incorporada'.
    Referente a interação, usei o básico de JS mesmo (Apenas para validação de valor)

--- desafios/des004/script.js
+++ desafios/des004/script.js
@@ -1,15 +1,52 @@
-function comprar() {
 
 
-    let pr1 = document.getElementById('prod1');
 
-    let pr2 = document.getElementById('prod2');
-    let pr3 = document.getElementById('prod3');
-    let pr4 = document.getElementById('prod4');
+let botoesProd = document.getElementsByClassName('produto');
 
-    pr1 = 100;
-    pr2 = 200;
-    pr3 = 300;
-    pr4 = 400;
+for (let i = 0; i < botoesProd.length; i++) {
+    botoesProd[i].addEventListener('click', function () {
+
+        //Obtendo o preço associado ao botão clicado
+        var preco = this.getAttribute('data-preco');
+
+        //Executando a função para exibir o preço
+        mostrarPreco(preco);
+    });
+}
+
+//Função para exibir o preço
+function mostrarPreco(preco) {
+    window.alert(`O preço do produto é $${preco}. Continuar com a compra?`);
+
+    let resposta = Number.parseFloat(prompt('Qual a quantia de dinheiro que você tem?'))
+
+    let troco = resposta - preco;
+
+    if (isNaN(resposta) || resposta == 0) {
+        alert(`Desculpe, coloque o valor que deseja, para fazer a validação do pagamento!`);
+
+    }else if (resposta >= preco) {
+        alert(`Compra confirmada.Obrigado pela preferência! 
+Aqui está seu troco: $${troco.toFixed(2)}`);
+    }else{
+        alert('Compra negada. Saldo insuficiente, ou falta de crédito!');
+    }
+
+    
 
 }
+
+/* 
+(preco >= resposta) {
+    alert(`Compra negada, falta de saldo`);
+else if (resposta == preco){
+
+    alert(`Compra efetuada com sucesso. Obrigado pela preferência`);
+
+} else {
+    alert(`Compra efetuada com sucesso. Obrigado pela preferência. Seu troco: ${troco}`);
+}
+
+*/
+
+
